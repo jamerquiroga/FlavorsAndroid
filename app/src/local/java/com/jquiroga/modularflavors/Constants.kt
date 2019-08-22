5fc8dd427b5d301aeb7734274a0be82364161c05
Jamer Quiroga Llanos - EVERIS <jquiroga@bcp.com.pe>
2019-08-22 16:18:27 -0500

    Initial commit

---
+++ app/src/local/java/com/jquiroga/modularflavors/Constants.kt
@@ -0,0 +1,9 @@
+package com.jquiroga.modularflavors
+
+class Constants {
+
+    companion object {
+        const val FLAVOR_NAME: String = BuildConfig.FLAVOR
+    }
+
+}
---
+++ app/src/uat/java/com/jquiroga/modularflavors/Constants.kt
@@ -0,0 +1,9 @@
+package com.jquiroga.modularflavors
+
+class Constants {
+
+    companion object {
+        val FLAVOR_NAME: String = BuildConfig.FLAVOR
+    }
+
+}
