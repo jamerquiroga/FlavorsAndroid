9683485572ac302769879fbcc7712b2f80f862a2
Jamer Quiroga Llanos - EVERIS <jquiroga@bcp.com.pe>
2019-08-23 14:49:55 -0500

    Add flavor.gradle file

--- app/src/main/java/com/jquiroga/modularflavors/MainActivity.kt
+++ app/src/main/java/com/jquiroga/modularflavors/MainActivity.kt
@@ -2,7 +2,7 @@ package com.jquiroga.modularflavors
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.jquiroga.samplelibrary.CoreConstants
+//import com.jquiroga.samplelibrary.CoreConstants
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,9 +13,9 @@ class MainActivity : AppCompatActivity() {
 
         tvWelcomeMessage.text = Constants.FLAVOR_NAME
 
-        tvSampleValueLibrary.text = CoreConstants.SAMPLE_VALUE
+        //tvSampleValueLibrary.text = CoreConstants.SAMPLE_VALUE
 
-        tvFlavorLibrary.text = CoreConstants.LIBRARY_FLAVOR
+        //tvFlavorLibrary.text = CoreConstants.LIBRARY_FLAVOR
 
     }
 }
